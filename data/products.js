f7c3b51a754a756be9dc6ba066431d27697c2b2e
NeilCodex <batuigas.lien@gmail.com>
2025-12-10 13:11:02 +0800

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -105,10 +105,13 @@ export function loadProductsFetch() {
     });
 
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
 }
+
 /*
 loadProductsFetch().then(() => {
   console.log('next step');
@@ -129,6 +132,10 @@ export function loadProducts(fun) {
     console.log('load products');
 
     fun();
+  });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
